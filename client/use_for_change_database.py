af08374e587a8b0af7f696c41c1b47ed92d5c1f0
inon_Desktop <y.igeorgi@gmail.com>
2023-03-11 21:58:11 +0200

    stap to use the search opshen

--- client/use_for_change_database.py
+++ client/use_for_change_database.py
@@ -20,24 +20,3 @@
 #
 # mydb.commit()
 
-def CheckID(ID):
-    if len(ID) != 9:
-        return False
-
-    try:
-        id = list(map(int, ID))
-    except:
-        return False
-
-    counter = 0
-
-    for i in range(9):
-        id[i] *= (i % 2) + 1
-        if id[i] > 9:
-            id[i] -= 9
-        counter += id[i]
-
-    if counter % 10 == 0:
-        return True
-    else:
-        return False
